0339858e2b5c82354d4a1d2a95d65b881a8842d6
StibaVII <tim.sulavuori@luukku.com>
2016-04-28 09:09:08 +0800

    Dunno What?

---
+++ Assets/Scripts/Generate.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generate : MonoBehaviour {
+
+    public GameObject enemies;
+	public GameObject Canvas;
+    public float speed = 1f;
+    public float spawnTimer = 1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("CreateObstacle", speed, spawnTimer);
+    }
+
+    void CreateObstacle()
+    {
+        GameObject go = Instantiate(enemies, new Vector3 (0,0,0), Quaternion.identity) as GameObject; 
+		go.transform.parent = GameObject.Find("Canvas").transform;
+    }
+}
---
+++ Assets/Scripts/MoveEnemies.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveEnemies : MonoBehaviour {
+public GameObject enemies;
+public Vector3 scale;
+	// Use this for initialization
+	void Start () {
+			enemies.transform.localScale = (scale);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
