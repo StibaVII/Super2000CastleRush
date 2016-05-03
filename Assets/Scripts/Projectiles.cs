fa143ae85e06d57806e83f22887797a9731275e4
StibaVII <tim.sulavuori@luukku.com>
2016-05-03 09:40:30 +0800

    Shoot to mouse

--- Assets/Scripts/Projectiles.cs
+++ Assets/Scripts/Projectiles.cs
@@ -8,6 +8,7 @@ public class Projectiles : MonoBehaviour {
     public Canvas canvas;
     public float scalex = 15f;
     public float scaley = 3f;
+	public float amount = 2000000f;
     public GameObject hero;
 
 	// Use this for initialization
@@ -28,9 +29,12 @@ public class Projectiles : MonoBehaviour {
         GameObject arrowsSpawned = Instantiate(arrow);
         arrowsSpawned.transform.SetParent(canvas.transform);
         arrowsSpawned.GetComponent<Rigidbody2D>().velocity = velocity;
+		        Vector3 sp = Camera.main.WorldToScreenPoint(transform.position);
+				Vector3 dir = (Input.mousePosition - sp).normalized;
         Vector3 spawnScale = new Vector3(scalex, scaley);
         Vector3 spawnPosition = new Vector3(hero.transform.localPosition.x, hero.transform.localPosition.y);
         arrowsSpawned.transform.localScale = spawnScale;
         arrowsSpawned.transform.localPosition = spawnPosition;
+		arrowsSpawned.GetComponent<Rigidbody2D>().AddForce (dir * amount);
     }
 }
